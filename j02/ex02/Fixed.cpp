827fc93f3a25773a072bf6eea9a5717e251f9d84
Pierre FICHEPOIL <pfichepo@e2r9p14.42.fr>
2018-05-31 11:16:48 +0200

    finished ex02

--- j02/ex02/Fixed.cpp
+++ j02/ex02/Fixed.cpp
@@ -6,30 +6,30 @@ const int Fixed::_bit = 8;
 
 Fixed::Fixed() : _raw(0)
 {
-	std::cout << "Default constructor called" << std::endl;
+	//std::cout << "Default constructor called" << std::endl;
 }
 
 Fixed::~Fixed()
 {
-	std::cout << "Destructor called" << std::endl;
+	//std::cout << "Destructor called" << std::endl;
 }
 
 Fixed::Fixed(Fixed const &source)
 {
-	std::cout << "Copy constructor called" << std::endl; 
+	//std::cout << "Copy constructor called" << std::endl; 
 	*this = source;
 }
 
 
 Fixed::Fixed(int const n)
 {
-	std::cout << "Int constructor called" << std::endl;
+	//std::cout << "Int constructor called" << std::endl;
 	setRawBits(n << this->_bit); // on bouge l'int à gauche de la virgule fixe
 }
 
 Fixed::Fixed(float const n)
 {
-	std::cout << "Float constructor called" << std::endl;
+	//std::cout << "Float constructor called" << std::endl;
 	this->_raw = roundf(n * (1 << this->_bit)); // on peut pas juste décaler vue c'est une float, bon on perd de la précision là par contre
 	return;
 }
@@ -42,26 +42,159 @@ float	Fixed::toFloat(void) const{
 	return ((float)this->_raw / (1 << this->_bit)); // pareil, on fait l'inverse
 }
 
-int Fixed::getRawBits( void ) const
+int 	Fixed::getRawBits( void ) const
 {
-	std::cout << "getRawBits member function called" << std::endl;
+	//std::cout << "getRawBits member function called" << std::endl;
 	return (this->_raw);
 }
 
-void Fixed::setRawBits( int const raw )
+void 	Fixed::setRawBits( int const raw )
 {
 	this->_raw = raw;
 }
 
 
-Fixed & Fixed::operator=(const Fixed & rhs)
-{
-	std::cout << "Assignation operator called" << std::endl;
-	this->_raw= rhs._raw;
-	return (*this);
-}
+
 
 std::ostream &operator<<(std::ostream &o, Fixed const &rhs) {
 	o << rhs.toFloat();
 	return(o);
 }
+
+Fixed &Fixed::operator=(Fixed const &rhs)
+{
+	//std::cout << "Assignation operator called" << std::endl;
+	this->_raw = rhs._raw;
+	return (*this);
+}
+
+
+bool		Fixed::operator>(Fixed const & rhs) const
+{
+	return (this->getRawBits() > rhs.getRawBits());
+}
+
+bool		Fixed::operator<(Fixed const & rhs) const
+{
+	return (this->getRawBits() < rhs.getRawBits());
+}
+
+bool		Fixed::operator>=(Fixed const & rhs) const
+{
+	return (this->getRawBits() >= rhs.getRawBits());
+}
+
+bool		Fixed::operator<=(Fixed const & rhs) const
+{
+	return (this->getRawBits() <= rhs.getRawBits());
+}
+
+bool		Fixed::operator==(Fixed const & rhs) const
+{
+	return (this->getRawBits() == rhs.getRawBits());
+}
+
+bool		Fixed::operator!=(Fixed const & rhs) const
+{
+	return (this->getRawBits() != rhs.getRawBits());
+}
+
+
+Fixed 		&Fixed::operator++(void)
+{
+	this->_raw++;
+	return (*this);
+}
+
+
+Fixed 		&Fixed::operator--(void)
+{
+	this->_raw--;
+	return (*this);
+}
+
+/*
+** gros hack, on peut pas dire "attends une instruction avant de faire ce que t'as à faire"
+** donc on modifie ce quon doit modifier mais on retourne une copie de la valeur d'avant.
+*/ 
+
+Fixed		Fixed::operator++(int) 
+{
+	Fixed	groshack;
+
+	groshack = *this; // copie implicide
+	this->_raw++;
+	return (groshack);
+}
+
+Fixed 		Fixed::operator--(int)
+{
+	Fixed	groshack;
+
+	groshack = *this;
+	this->_raw--;
+	return (groshack);
+}
+
+
+Fixed		Fixed::operator+(Fixed const & rhs)
+{
+	Fixed output;
+	output._raw = this->_raw + rhs._raw;
+	return (output);
+}
+
+
+Fixed		Fixed::operator-(Fixed const & rhs)
+{
+	Fixed output;
+	output._raw = this->_raw - rhs._raw; // pas compliqué
+	return (output);
+}
+
+Fixed		Fixed::operator*(Fixed const & rhs)
+{
+	Fixed output;
+	//output._raw =  ((this->_raw >> this->_bit) * (rhs._raw >> this->_bit)) << this->_bit ; // perde de precision donc on oublie ca
+	output._raw = (this->_raw * rhs._raw) >> this->_bit;
+	return (output);
+}
+
+Fixed		Fixed::operator/(Fixed const & rhs)
+{
+	Fixed output;
+	output._raw =  (this->_raw / rhs._raw) >> this->_bit; // pareil
+	return (output);
+}
+
+
+
+Fixed 	&Fixed::min(Fixed & val1, Fixed & val2)
+{
+	if (val1._raw < val2._raw)
+		return (val1);
+	return (val2);
+}
+
+Fixed 	&Fixed::max(Fixed & val1, Fixed & val2)
+{
+	if (val1._raw > val2._raw)
+		return (val1);
+	return (val2);
+}
+
+Fixed const &Fixed::min(Fixed const & val1, Fixed const & val2)
+{
+	if (val1._raw < val2._raw)
+		return (val1);
+	return (val2);
+}
+
+Fixed const &Fixed::max(Fixed const & val1, Fixed const & val2)
+{
+	if (val1._raw > val2._raw)
+		return (val1);
+	return (val2);
+}
+
+
--- j02/ex02/Fixed.hpp
+++ j02/ex02/Fixed.hpp
@@ -14,6 +14,8 @@ public:
 	void setRawBits( int const raw );
 	int toInt(void) const;
 	float toFloat(void) const;
+
+	// putain de journée de merde
 	bool			operator>(Fixed const & rhs) const;
 	bool			operator<(Fixed const & rhs) const;
 	bool			operator>=(Fixed const & rhs) const;
@@ -23,19 +25,20 @@ public:
 
 	Fixed 			&operator=(const Fixed & rhs);
 	Fixed 			&operator++(void);
+	Fixed 			operator++(int);
+	Fixed 			&operator--(void);
+	Fixed 			operator--(int); // post
 	Fixed			operator+(Fixed const & rhs);
 	Fixed			operator-(Fixed const & rhs);
 	Fixed			operator*(Fixed const & rhs);
 	Fixed			operator/(Fixed const & rhs);
 
-	Fixed			operator++(int);
 
 	static Fixed 	&min(Fixed & val1, Fixed & val2);
 	static Fixed 	&max(Fixed & val1, Fixed & val2);
 
 	static Fixed const &min(Fixed const & val1, Fixed const & val2);
 	static Fixed const &max(Fixed const & val1, Fixed const & val2);
-
 
 
 private:
