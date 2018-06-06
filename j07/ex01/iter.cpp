0d7c190d0cb405f321d324fe40e5c596215460f1
Pierre Fichepoil <ExtReMLapin@users.noreply.github.com>
2018-06-06 21:48:16 +0200

    Update iter.cpp

--- j07/ex01/iter.cpp
+++ j07/ex01/iter.cpp
@@ -1,29 +1,5 @@
-
 #include <iostream>
 #include <unistd.h>
-
-
-
-
-
-template< typename T , typename U>
-void	iter(T *array, int lenght, void (*func) (U )) {
-
-	for (int i = 0; i < lenght; i++) {
-		func(array[i]);
-	}
-	return ;
-}
-
-
-
-static void ft_putchar(char c)
-{
-	write(1, &c, 1);
-}
-
-
-
 
 
 class Awesome
@@ -37,11 +13,20 @@ class Awesome
 
 std::ostream & operator<<( std::ostream & o, Awesome const & rhs ) { o << rhs.get(); return o; }
 
+
 template< typename T >
 void print( T const & x ) { std::cout << x << std::endl; return; }
 
 
 
+template< typename T>
+void	iter(T const *array, int lenght, void (*func) (T const & x )) {
+
+	for (int i = 0; i < lenght; i++) {
+		func(array[i]);
+	}
+	return ;
+}
 
 int main(void)
 {
@@ -49,11 +34,7 @@ int main(void)
 	char dada[] = "LIBEREW LE TIBAY";
 	int tab[] = { 0, 1, 2, 3, 4 }; // <--- J'ai jamais compris pourquoi on peut pas ecrire int[] tab. Ca aurait plus de sens vous trouvez pas ?
 	Awesome tab2[5];
-	iter( dada, 17, ft_putchar);
-	iter( tab, 5, ft_putchar );
-	iter( tab2, 5, ft_putchar );
-
-
+	iter( dada, 16, print);
+	iter( tab, 5, print );
+	iter( tab2, 5, print );
 }
-
-
