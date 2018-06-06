5fd69ef1a7c1594c1ec69b1e3e6287b90d7a5479
Pierre FICHEPOIL <pfichepo@e2r9p14.42.fr>
2018-06-06 16:10:43 +0200

    d

--- j07/ex01/iter.cpp
+++ j07/ex01/iter.cpp
@@ -3,10 +3,15 @@
 #include <unistd.h>
 
 
-template<typename T, typename U>
-void	iter( T* array, size_t length, void (*f)(U) )
+
+
+
+template<typename T>
+void	iter( T* array, int length, void (*f)(T) )
 {
 	size_t len = 0;
+	if (length < 0)
+		return;
 	while (len < length)
 	{
 		f(array[len]);
@@ -16,10 +21,33 @@ void	iter( T* array, size_t length, void (*f)(U) )
 }
 
 
+
+
 static void ft_putchar(char c)
 {
 	write(1, &c, 1);
 }
+
+
+
+
+
+class Awesome {
+
+public:
+Awesome( void ) : _n( 42 ) { return; }
+int get( void ) const { return this->_n; }
+
+private:
+int _n;};
+
+
+std::ostream & operator<<( std::ostream & o, Awesome const & rhs ) { o << rhs.get(); return o; }
+
+template< typename T >
+void print( T const & x ) { std::cout << x << std::endl; return; }
+
+
 
 
 int main(void)
@@ -27,6 +55,16 @@ int main(void)
 
 	char dada[] = "LIBEREW LE TIBAY";
 
-	iter(dada, static_cast<size_t>(17), ft_putchar);
+	iter(dada, 17, ft_putchar);
+
+
+int tab[] = { 0, 1, 2, 3, 4 }; // <--- J'ai jamais compris pourquoi on peut pas ecrire int[] tab. Ca aurait plus de sens vous trouvez pas ?
+Awesome tab2[5];
+
+iter( tab, 5, print );
+iter( tab2, 5, print );
+
 
 }
+
+
